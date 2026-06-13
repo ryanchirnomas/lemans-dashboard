8d0e5da4e3543e889d128734f8fd471eacf6ae86
ryanchirnomas <ryanchirnomas@mac.com>
2026-06-13 11:14:00 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,235 @@
+const accounts = [
+
+{
+name:"Ferrari Hypercar",
+team:"Ferrari AF Corse",
+class:"Hypercar",
+x:"https://x.com/FerrariHypercar",
+instagram:"https://instagram.com/ferrarihypercar"
+},
+
+{
+name:"Porsche Penske Motorsport",
+team:"Porsche Penske",
+class:"Hypercar",
+x:"https://x.com/PorscheRaces",
+instagram:"https://instagram.com/porschemotorsport"
+},
+
+{
+name:"Toyota Gazoo Racing",
+team:"Toyota",
+class:"Hypercar",
+x:"https://x.com/TGR_WEC",
+instagram:"https://instagram.com/toyotagazooracing"
+},
+
+{
+name:"Cadillac Hertz Team JOTA",
+team:"Cadillac",
+class:"Hypercar",
+x:"https://x.com/CadillacVSeries",
+instagram:"https://instagram.com/cadillacvseries"
+},
+
+{
+name:"BMW M Team WRT",
+team:"BMW",
+class:"Hypercar",
+x:"https://x.com/BMWMotorsport",
+instagram:"https://instagram.com/bmwmmotorsport"
+},
+
+{
+name:"Peugeot Sport",
+team:"Peugeot",
+class:"Hypercar",
+x:"https://x.com/PeugeotSport",
+instagram:"https://instagram.com/peugeotsport"
+},
+
+{
+name:"Alpine Endurance Team",
+team:"Alpine",
+class:"Hypercar",
+x:"https://x.com/AlpineRacing",
+instagram:"https://instagram.com/alpineracing"
+},
+
+{
+name:"Aston Martin THOR Team",
+team:"Aston Martin",
+class:"Hypercar",
+x:"https://x.com/AstonMartin",
+instagram:"https://instagram.com/astonmartin"
+},
+
+{
+name:"United Autosports",
+team:"United Autosports",
+class:"LMP2",
+x:"https://x.com/UAutosports",
+instagram:"https://instagram.com/unitedautosports"
+},
+
+{
+name:"Inter Europol Competition",
+team:"Inter Europol",
+class:"LMP2",
+x:"https://x.com/IE_Competition",
+instagram:"https://instagram.com/intereuropolcompetition"
+},
+
+{
+name:"AO by TF",
+team:"AO Racing",
+class:"LMP2",
+x:"https://x.com/AORacingUSA",
+instagram:"https://instagram.com/aoracingusa"
+},
+
+{
+name:"Algarve Pro Racing",
+team:"APR",
+class:"LMP2",
+x:"https://x.com/AlgarvePro",
+instagram:"https://instagram.com/algarveproracing"
+},
+
+{
+name:"AF Corse LMGT3",
+team:"AF Corse",
+class:"LMGT3",
+x:"https://x.com/AFCorse",
+instagram:"https://instagram.com/afcorse_official"
+},
+
+{
+name:"Manthey Racing",
+team:"Manthey",
+class:"LMGT3",
+x:"https://x.com/Manthey_Racing",
+instagram:"https://instagram.com/manthey_racing"
+},
+
+{
+name:"TF Sport",
+team:"TF Sport",
+class:"LMGT3",
+x:"https://x.com/TFSport",
+instagram:"https://instagram.com/tfsport"
+},
+
+{
+name:"Heart of Racing",
+team:"Heart of Racing",
+class:"LMGT3",
+x:"https://x.com/HeartofRacing",
+instagram:"https://instagram.com/heartofracingteam"
+},
+
+{
+name:"Iron Lynx",
+team:"Iron Lynx",
+class:"LMGT3",
+x:"https://x.com/IronLynx_",
+instagram:"https://instagram.com/ironlynx"
+},
+
+{
+name:"Robert Kubica",
+team:"Ferrari AF Corse",
+class:"Driver",
+x:"https://x.com/TeamKubica",
+instagram:"https://instagram.com/robertkubica_real"
+},
+
+{
+name:"Kevin Estre",
+team:"Porsche Penske",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/kevinestre"
+},
+
+{
+name:"Antonio Giovinazzi",
+team:"Ferrari AF Corse",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/antoniogiovinazzi"
+},
+
+{
+name:"Nicklas Nielsen",
+team:"Ferrari AF Corse",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/nicklasnielsenofficial"
+},
+
+{
+name:"James Calado",
+team:"Ferrari AF Corse",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/jamescalado"
+},
+
+{
+name:"Antonio Fuoco",
+team:"Ferrari AF Corse",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/antoniofuoco_"
+},
+
+{
+name:"Laurens Vanthoor",
+team:"Porsche Penske",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/laurensvanthoor"
+},
+
+{
+name:"Matt Campbell",
+team:"Porsche Penske",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/mattcampbellracing"
+},
+
+{
+name:"Earl Bamber",
+team:"Cadillac",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/earlbamber"
+},
+
+{
+name:"Alex Lynn",
+team:"Cadillac",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/alexlynnracer"
+},
+
+{
+name:"Raffaele Marciello",
+team:"BMW",
+class:"Driver",
+x:"",
+instagram:"https://instagram.com/raffaelemarciello"
+},
+
+{
+name:"Valentino Rossi",
+team:"BMW WRT",
+class:"Driver",
+x:"https://x.com/ValeYellow46",
+instagram:"https://instagram.com/valeyellow46"
+}
+
+];
